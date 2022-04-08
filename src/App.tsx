c42851b1b47ad3e0934390e7feacafbe23dc0ddd
slavaMatyuk <vyacheslav.matyuk@innowise-group.com>
2022-04-08 13:50:35 +0300

    test scenario init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { memo, useEffect, useState } from 'react';
+import { TextField } from '@mui/material';
+import './styles.css';
+import { INITIAL_LIST, LIST_SIZE, TIME_INTERVAL } from './constants';
 
-function App() {
+const getCollection = (name: string) =>
+  fetch(
+    `https://itunes.apple.com/search?term=${name}`
+  )
+    .then((res) => res.json())
+    .then((data) =>
+      data.results
+        .map((item: any) => item.collectionName)
+        .filter((item: string) => Boolean(item))
+        .sort((a: string, b: string) => a.localeCompare(b))
+    );
+
+const List = memo(({ list }: any) => {
+  const [currentList, setCurrentList] = useState(list);
+
+  useEffect(() => {
+    setCurrentList((list: []) => [
+      ...list.slice(0, LIST_SIZE),
+      ...list
+    ]);
+  }, [list]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentList((list: []) => {
+        const newList = list.slice(1);
+        return newList.length >= LIST_SIZE ? newList : [...newList, ...list];
+      });
+    }, TIME_INTERVAL);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [list]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="list-container">
+      {currentList.slice(0, LIST_SIZE).map((item: string, i: number) => (
+        <div key={i} className="list-item">
+          {item}
+        </div>
+      ))}
     </div>
   );
-}
+});
 
-export default App;
+export default function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [list, setList] = useState(INITIAL_LIST);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => setInputValue(e.target.value);
+
+  useEffect(() => {
+    if (inputValue.length > 1) {
+      getCollection(inputValue).then((names) => {
+        if (names.length > 0) {
+          setList([...names].slice(0, LIST_SIZE));
+        }
+      });
+    }
+  }, [inputValue]);
+
+  return (
+    <div className="App">
+      <TextField
+        id="outlined-basic"
+        label="Search Band"
+        variant="outlined"
+        className="searchInput"
+        type="search"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <List list={list}/>
+    </div>
+  );
+};
---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const TIME_INTERVAL = 1000;
+export const LIST_SIZE = 5;
+export const INITIAL_LIST = ['A', 'B', 'C', 'D', 'E'];
